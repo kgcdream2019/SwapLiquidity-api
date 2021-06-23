cfc072d93c16d0daba9263799cf15acb0e46ba49
dream2019 <kgc.dream2019@gmail.com>
2021-06-23 21:17:05 +0800

    ttt

--- src/404.ts
+++ src/404.ts
@@ -2,5 +2,5 @@ import { NowRequest, NowResponse } from '@now/node'
 import { returnError } from './utils/response'
 
 export default async function(req: NowRequest, res: NowResponse): Promise<void> {
-  returnError(res, 404, 'Invalid URL')
+  returnError(res, 404, 'Invalid URL ttt')
 }
