b1eee1f7b7d4b6bc80504254429823f7f3f80286
kgcdream2019 <kgc.dream2019@gmail.com>
2022-01-19 23:06:01 +0800

    update url

--- src/blocks/client.ts
+++ src/blocks/client.ts
@@ -6,7 +6,7 @@ import fetch from 'node-fetch'
 export const blockClient = new ApolloClient({
   link: new HttpLink({
     fetch,
-    uri: 'https://subgraph.swapliquidity.org/subgraphs/name/julswap/bsc-blocks'
+    uri: 'http://127.0.0.1:8000/subgraphs/name/julswap/bsc-blocks'
   }),
   cache: new InMemoryCache()
 })
